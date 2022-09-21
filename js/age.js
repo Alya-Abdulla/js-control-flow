3e93489af7186b54cc1ddb895f0e840d6b4a8c55
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-09-21 09:12:36 +0300

    larger Done

--- js/age.js
+++ js/age.js
@@ -1,1 +1,14 @@
 
+let a = 10
+let b = 20
+if (a>b){
+console.log("The larger number of "+a+" and "+b+" is "+ a)
+}
+else if(a<b){
+    console.log("The larger number of "+a+" and "+b+" is " +b)
+
+}
+else{
+  console.log("Both are equal")
+}
+
