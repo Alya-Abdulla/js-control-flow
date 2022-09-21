3fb8e775e92f4ba0d2eda99e5c0de41b4c369771
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-09-21 10:18:34 +0300

    Bouns Done

---
+++ js/larger.js
@@ -0,0 +1,17 @@
+
+
+// Write a program that reads two Number variables and prints which one is greater. (e.g. "The larger number of 7 and 3 is 7")
+
+
+let a = 10
+let b = 20
+if (a>b){
+console.log("The larger number of "+a+" and "+b+" is "+ a)
+}
+else if(a<b){
+    console.log("The larger number of "+a+" and "+b+" is " +b)
+
+}
+else{
+  console.log("Both are equal")
+}
